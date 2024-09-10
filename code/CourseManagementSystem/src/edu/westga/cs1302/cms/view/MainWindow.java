6e57caac6982296fdfd1f609bd2d1c26ce9abcf8
Lucas <ldeniso1@my.westga.edu>
2024-09-10 17:12:55 -0400

    Finished freature 1. Added remove function and a funtion to view grade

--- code/CourseManagementSystem/src/edu/westga/cs1302/cms/view/MainWindow.java
+++ code/CourseManagementSystem/src/edu/westga/cs1302/cms/view/MainWindow.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 /** Code behind for the MainWindow of the application
  * 
@@ -16,33 +17,54 @@ public class MainWindow {
     @FXML private TextField name;
     @FXML private ListView<Student> students;
     @FXML private TextField grade;
+    @FXML private TextArea displayGradeArea;
 
     @FXML
     void addStudent(ActionEvent event) {
     	
     	String studentName = this.name.getText();
     	try {
-    		int grade = Integer.parseInt(this.grade.getText());
+        	int grade = Integer.parseInt(this.grade.getText());
     		Student student = new Student(studentName, grade);
     		this.students.getItems().add(student);
-    	} catch (NumberFormatException error) {
+    	} catch (NumberFormatException errorNum1) {
     		Alert errorPopup = new Alert(Alert.AlertType.ERROR);
-    		errorPopup.setContentText("Unable to create student: " + error.getMessage() + ". Please reenter grade and try again");
+    		errorPopup.setContentText("Unable to create student: " + errorNum1.getMessage() + ". Please reenter grade and try again");
     		errorPopup.showAndWait();
-    	} catch (IllegalArgumentException error) {
+    	} catch (IllegalArgumentException errorNum2) {
     		Alert errorPopup = new Alert(Alert.AlertType.ERROR);
-    		errorPopup.setContentText("Unable to create student: " + error.getMessage() + ". Please reenter name and try again");
+    		errorPopup.setContentText("Unable to create student: " + errorNum2.getMessage() + ". Please reenter name and try again");
     		errorPopup.showAndWait();
     	}  
     }
 
     @FXML
     void removeStudent(ActionEvent event) {
-    	
+    	Student student = this.students.getSelectionModel().getSelectedItem();
+    	if (student != null) {
+    		this.students.getItems().remove(student);
+    	} else {
+    		Alert errorPopup = new Alert(Alert.AlertType.ERROR);
+    		errorPopup.setContentText("No student selected. Unable to remove.");
+    		errorPopup.showAndWait();
+    	}
+    }
+    
+    @FXML
+    void displayGrade(ActionEvent event) {
+    	Student student = this.students.getSelectionModel().getSelectedItem();
+    	if (student != null) {
+    		this.displayGradeArea.setText(String.valueOf(student.getGrade()));
+    	} else {
+    		Alert errorPopup = new Alert(Alert.AlertType.ERROR);
+    		errorPopup.setContentText("No student selected. Unable to view grade.");
+    		errorPopup.showAndWait();
+    	}
     }
 
     @FXML
     void initialize() {
+    	assert this.displayGradeArea != null : "fx:id=\"displayGradeArea\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.students != null : "fx:id=\"students\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.grade != null : "fx:id=\"grade\" was not injected: check your FXML file 'MainWindow.fxml'.";
