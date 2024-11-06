5b2b07b8bb233d52e2679e931a27818d94a1add9
Lucas <ldeniso1@my.westga.edu>
2024-11-05 20:45:59 -0500

    lab7 created viewmodel
    created a viewmodel for the mainWindow but have not tested it

---
+++ code/starterL7/Lab 7/src/edu/westga/cs1302/password_generator/test/password_generator/TestPasswordViewModel.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.password_generator.test.password_generator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPasswordViewModel {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
