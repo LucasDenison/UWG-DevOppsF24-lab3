2eb106372d1fcc2e326ace3b31994bab0dd7b5d6
Lucas <ldeniso1@my.westga.edu>
2024-11-22 21:59:58 -0500

    Task info
    Created a task class that stores the information of the task. Aslo created test to make sure task class works.

---
+++ code/starterP3/Project 3/src/edu/westga/cs1302/project3/test/TestTask.java
@@ -0,0 +1,41 @@
+package edu.westga.cs1302.project3.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.project3.model.Task;
+
+class TestTask {
+
+	@Test
+	void testIfTitleNull() {
+		assertThrows(IllegalArgumentException.class, ()->{new Task(null,"bruh");});
+	}
+
+	@Test
+	void testIfTitleEmpty() {
+		assertThrows(IllegalArgumentException.class, ()->{new Task("","bruh");});
+	}
+	
+	@Test
+	void testIfDescriptionNull() {
+		assertThrows(IllegalArgumentException.class, ()->{new Task("bruh", null);});
+	}
+	
+	@Test
+	void testIfDescriptionEmpty() {
+		assertThrows(IllegalArgumentException.class, ()->{new Task("bruh", "");});
+	}
+	
+	@Test
+	void testIfTaskIsValid() {
+		Task task = new Task("bruh", "bruh");
+		
+		String taskTitle = task.getTitle();
+		String taskDescription =  task.getDescription();
+		
+		assertEquals(taskTitle, "bruh");
+		assertEquals(taskDescription, "bruh");
+	}
+}
