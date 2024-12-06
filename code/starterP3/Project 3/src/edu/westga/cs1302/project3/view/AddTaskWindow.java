0f904cd42d0b3c82d1fe4fdeadaaa0a62373bfeb
Lucas <ldeniso1@my.westga.edu>
2024-12-05 21:51:36 -0500

    cancel button
    added a funtion to the cancel button in AddTaskWindow to close the AddTaskWindow when pressed.

--- code/starterP3/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
+++ code/starterP3/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
@@ -1,10 +1,25 @@
 package edu.westga.cs1302.project3.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
 /** Codebehind for the Add Task Window of the application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class AddTaskWindow {
+	@FXML
+    private Button cancelButton;
+	
+	@FXML
+	void initialize() {
+		this.cancelButton.setOnAction(
+				(event) -> {
+					((Node) (event.getSource())).getScene().getWindow().hide();
+				}
+			);
+	}
 
 }
