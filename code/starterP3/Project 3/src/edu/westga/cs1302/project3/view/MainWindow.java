405ece2bf5707a21a7961499ff70b631a0d83af1
Lucas <ldeniso1@my.westga.edu>
2024-12-05 21:37:11 -0500

    open AddTaskWindow
    added a method that opens the add task window

--- code/starterP3/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
+++ code/starterP3/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
@@ -1,14 +1,23 @@
 package edu.westga.cs1302.project3.view;
 
 import java.io.File;
+import java.io.IOException;
 
+import edu.westga.cs1302.project3.Main;
+import edu.westga.cs1302.project3.view.AddTaskWindow;
 import edu.westga.cs1302.project3.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -31,6 +40,9 @@ public class MainWindow {
 	    private MenuItem loadMenuItem;
 	    @FXML
 	    private MenuItem saveMenuItem;
+	    @FXML
+	    private Button addTaskButton;
+	    
 	    
 	    private ViewModel vm;
 	    
@@ -64,6 +76,12 @@ public class MainWindow {
 	    				this.saveFile();
 	    			}
 	    	);
+	    	
+	    	this.addTaskButton.setOnAction(
+	    			(event) -> {
+	    				this.displayAddTasksScreen();
+	    			}
+	    		);
 	    }
 	    
 	    private void showAbout() {
@@ -126,4 +144,23 @@ public class MainWindow {
 				alert.showAndWait();
 	    	}
 	    }
+	    
+	    private void displayAddTasksScreen() {
+	    	FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource(Main.ADD_TASK_WINDOW));
+			try {
+				loader.load();
+				Parent parent = loader.getRoot();
+				Scene scene = new Scene(parent);
+				Stage setPropertyStage = new Stage();
+				setPropertyStage.setTitle(Main.ADD_TASK_TITLE);
+				setPropertyStage.setScene(scene);
+				setPropertyStage.initModality(Modality.APPLICATION_MODAL);
+				setPropertyStage.showAndWait();
+			} catch (IOException error) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText("Unable to load properties window.");
+				alert.showAndWait();
+			}
+		}
 }
