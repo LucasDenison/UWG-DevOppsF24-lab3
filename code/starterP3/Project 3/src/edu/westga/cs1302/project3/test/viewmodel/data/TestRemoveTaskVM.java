033aa70a9498bd056797591b5c5d49afc2b3191b
Lucas <ldeniso1@my.westga.edu>
2024-12-06 02:13:50 -0500

    remove task UI
    added funtionality to the UI that removes the selected item in the listview of tasks

---
+++ code/starterP3/Project 3/src/edu/westga/cs1302/project3/test/viewmodel/data/TestRemoveTaskVM.java
@@ -0,0 +1,40 @@
+package edu.westga.cs1302.project3.test.viewmodel.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.project3.model.Task;
+import edu.westga.cs1302.project3.model.TaskManager;
+import edu.westga.cs1302.project3.viewmodel.ViewModel;
+
+class TestRemoveTaskVM {
+
+	@Test
+	void testNullTask() {
+		ViewModel vm = new ViewModel();
+		vm.getSelectedTask().setValue(null);
+		
+		
+		assertThrows(IllegalArgumentException.class, ()->{vm.removeTask();});
+	}
+
+	@Test
+	void testNoTaskInList() {
+		ViewModel vm = new ViewModel();
+		vm.getSelectedTask();
+		
+		assertThrows(IllegalArgumentException.class, ()->{vm.removeTask();});
+	}
+	
+	@Test
+	void testRemoveOneTask() {
+		ViewModel vm = new ViewModel();
+		
+		vm.addTask("1:35", "AM");
+		vm.getSelectedTask().setValue(vm.getTasks().getValue().get(0));
+		vm.removeTask();
+		
+		assertEquals(1, vm.getTasks().getSize());
+	}
+}
